c6249929e21de8d06a919cf8f27fab89449bb65d
Mariia Pidisko <mahaves1990@gmail.com>
2022-03-23 15:28:38 +0200

    HomeWork7:Creating a Source model and forms for it

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,7 +1,10 @@
+from currency.forms import SourceForm
 from currency.models import ContactUs
 from currency.models import Rate
+from currency.models import Source
 
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 
 
 def rate_list(request):
@@ -12,6 +15,46 @@ def rate_list(request):
 def cont_list(request):
     contacts = ContactUs.objects.all()
     return render(request, 'cont_list.html', context={'contacts': contacts})
+
+
+def source_list(request):
+    sources = Source.objects.all()
+    return render(request, 'source_list.html', context={'sources': sources})
+
+
+def source_create(request):
+    if request.method == 'POST':
+        form = SourceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/source/list')
+    else:
+        form = SourceForm()
+
+    return render(request, 'source_create.html', context={'form': form})
+
+
+def source_update(request, pk):
+    instance = get_object_or_404(Source, pk=pk)
+
+    if request.method == 'POST':
+        form = SourceForm(request.POST, instance=instance)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/source/list')
+    else:
+        form = SourceForm(instance=instance)
+
+    return render(request, 'source_update.html', context={'form': form})
+
+
+def source_delete(request, pk):
+    instance = get_object_or_404(Source, pk=pk)
+    if request.method == 'POST':
+        instance.delete()
+        return HttpResponseRedirect('/source/list')
+    else:
+        return render(request, 'source_delete.html', context={'source': instance})
 
 
 def index(request):
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('rate/list/', currency_views.rate_list),
     path('', currency_views.index),
     path('cont/list/', currency_views.cont_list),
+    path('source/list/', currency_views.source_list),
+    path('source/create/', currency_views.source_create),
+    path('source/update/<int:pk>', currency_views.source_update),
+    path('source/delete/<int:pk>', currency_views.source_delete),
 ]
