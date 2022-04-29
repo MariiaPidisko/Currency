171258f1a9781333c93f480ea00e51fd5357a69a
Mariia Pidisko <mahaves1990@gmail.com>
2022-04-29 19:01:45 +0300

    Created SignUp form, model User, MyProfile, Choices

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,8 +1,10 @@
+from currency import model_choices as mch
+
 from django.db import models
 
 
 class Rate(models.Model):
-    type = models.CharField(max_length=10)   # noqa: A003
+    type = models.CharField(max_length=10, choices=mch.RateType.choices)   # noqa: A003
     source = models.CharField(max_length=20)
     created = models.DateTimeField(auto_now_add=True)
     buy = models.DecimalField(max_digits=10, decimal_places=2)
--- app/settings/settings.py
+++ app/settings/settings.py
@@ -142,7 +142,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_USE_TLS = True
 # EMAIL_PORT = 587
-EMAIL_HOST_USER = 'veselovamariia88@gmail.com'
+# EMAIL_HOST_USER = 'veselovamariia88@gmail.com'
 # EMAIL_HOST_PASSWORD = 'veselma123'
 
 if DEBUG:
